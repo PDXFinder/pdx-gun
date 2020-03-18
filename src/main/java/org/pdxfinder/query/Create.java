56310b90f2496fdb97fdbbd138983b68980f6a3f
sprintell <abayomi@sprintell.org>
2020-03-18 12:53:13 +0000

    Added feature to generate all needed sql create and insert queries to initialise postgres db

---
+++ src/main/java/org/pdxfinder/query/Create.java
@@ -0,0 +1,82 @@
+package org.pdxfinder.query;
+
+import com.healthmarketscience.sqlbuilder.dbspec.basic.DbColumn;
+import com.healthmarketscience.sqlbuilder.dbspec.basic.DbTable;
+import org.pdxfinder.constants.Column;
+import org.pdxfinder.constants.Location;
+import org.pdxfinder.constants.Table;
+import org.pdxfinder.loader.FileHandler;
+
+public class Create {
+
+    private SqlFacade sqlFacade;
+    private FileHandler fileHandler;
+
+
+    public Create() {
+
+        this.sqlFacade = new SqlFacade();
+        this.fileHandler = new FileHandler();
+    }
+
+
+    public DbTable resourceTable() {
+
+        DbTable resourceTable = sqlFacade.addTable(Table.RESOURCE);
+
+        // Add Columns to resource tables
+        sqlFacade.addPrimaryKey(resourceTable, Column.id);
+        sqlFacade.addVarchar(resourceTable, Column.name);
+
+        // Create Resource table Queries
+        String createTable = sqlFacade.createTableQuery(resourceTable);
+
+        // write query to File
+        fileHandler.write(createTable, Location.DESTINATION.get() , true );
+
+        return resourceTable;
+    }
+
+
+    public DbTable getGeneTable() {
+
+        DbTable geneTable = sqlFacade.addTable(Table.GENE);
+
+        // Add Columns to Gene table
+        sqlFacade.addPrimaryKey(geneTable, Column.id);
+        sqlFacade.addVarchar(geneTable, Column.name);
+
+        // Create Gene table Queries
+        String createTable = sqlFacade.createTableQuery(geneTable);
+
+        // write to file
+        fileHandler.write(createTable, Location.DESTINATION.get() , true );
+
+        return  geneTable;
+    }
+
+
+
+    public DbTable resourceUrlTable(DbTable geneTable, DbTable resourceTable) {
+
+        DbTable resourceUrlTable = sqlFacade.addTable(Table.RESOURCE_URL);
+
+        // Add Columns to resource_url table
+        sqlFacade.addPrimaryKey(resourceUrlTable, Column.id);
+        DbColumn geneId = sqlFacade.addIntegerColumn(resourceUrlTable, Column.gene_id);
+        DbColumn resourceId = sqlFacade.addIntegerColumn(resourceUrlTable, Column.resource_id);
+        sqlFacade.addVarchar(resourceUrlTable, Column.url);
+
+        // Add Foreign key Constraints
+        sqlFacade.addFKConstraint(resourceUrlTable, geneTable, geneId);
+        sqlFacade.addFKConstraint(resourceUrlTable, resourceTable, resourceId);
+
+        // Generate ResourceUrl table Queries
+        String createTable = sqlFacade.createTableQuery(resourceUrlTable);
+
+        // write to file
+        fileHandler.write(createTable, Location.DESTINATION.get() , true );
+
+        return resourceUrlTable;
+    }
+}
---
+++ src/main/java/org/pdxfinder/query/Insert.java
@@ -0,0 +1,79 @@
+package org.pdxfinder.query;
+
+import com.healthmarketscience.sqlbuilder.dbspec.basic.DbTable;
+import org.pdxfinder.constants.Location;
+import org.pdxfinder.constants.Resource;
+import org.pdxfinder.loader.FileHandler;
+
+import java.util.*;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class Insert {
+
+
+    private SqlFacade sqlFacade;
+    private Map<Integer, List<Object>> dataMap;
+    private FileHandler fileHandler;
+    
+    public Insert() {
+
+        this.sqlFacade = new SqlFacade();
+        this.fileHandler = new FileHandler();
+    }
+
+
+    public Map<Integer, List<Object>> resourceTable(DbTable resourceTable) {
+
+        dataMap = new LinkedHashMap<>();
+
+        // Resource table insert Queries
+        int counter = 0;
+        for (Resource resource : Resource.values()) {
+            dataMap.put(++counter, Collections.singletonList(resource.get()));
+        }
+
+        String query = sqlFacade.insertQuery(resourceTable, dataMap);
+        fileHandler.write(query, Location.DESTINATION.get() , true );
+
+        return dataMap;
+    }
+
+
+    public Map<Integer, List<Object>> geneTable(DbTable geneTable, Set<String> genes){
+
+        dataMap = new LinkedHashMap<>();
+
+        // Gene table insert Queries
+        AtomicInteger index = new AtomicInteger(0);
+        genes.forEach(s -> dataMap.put(index.incrementAndGet(), Collections.singletonList(s)));
+
+        // Generate Gene table Queries
+        String query = sqlFacade.insertQuery(geneTable, dataMap);
+        fileHandler.write(query, Location.DESTINATION.get() , true );
+
+        return dataMap;
+    }
+
+
+    public void resourceUrlTable(DbTable resourceUrlTable, Map<Integer, List<Object>> geneMap, Resource resource) {
+
+        dataMap = new LinkedHashMap<>();
+
+        AtomicInteger index = new AtomicInteger(0);
+        geneMap.forEach((key, value) -> {
+
+                            List<Object> rowData = new ArrayList<>();
+                            rowData.add(key);
+                            rowData.add(resource.ordinal() + 1);
+                            rowData.add("https://oncomx.org/searchview/?gene=" + value.get(0));
+                            dataMap.put(index.incrementAndGet(), rowData);
+                        }
+        );
+
+        String query = sqlFacade.insertQuery(resourceUrlTable, dataMap);
+        fileHandler.write(query, Location.DESTINATION.get(), true);
+    }
+
+}
+
+// https://civicdb.org/links/entrez_name/CXCR4
